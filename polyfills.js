0f8a0d62447407d73f73f2068ec5ab41db9cc3c8
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2020-06-24 02:29:05 +0200

    update build

--- polyfills.js
+++ polyfills.js
@@ -13166,8 +13166,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/yugoslavskiy/Dropbox/projects/react-navigator/nav-app/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/yugoslavskiy/Dropbox/projects/react-navigator/nav-app/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/yugoslavskiy/Dropbox/projects/demo/react-navigator/nav-app/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/yugoslavskiy/Dropbox/projects/demo/react-navigator/nav-app/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
